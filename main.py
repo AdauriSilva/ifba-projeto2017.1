7fcda3c7b017cdef17aa13d231ac9c7cea1c7da5
ADAURI BISPO SILVA <adauri.sys@gmail.com>
2017-06-23 12:46:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,73 @@
-#qpy:kivy
-'''
-Pictures demo
-=============
+#-*-coding:utf8;-*- apenas no Qpython mobile
+lista=""
+produtos=[]
+medicamentos=[]
+funcionarios=[]
+relatorios=[]
+def menu_principal(lista):
+    print("***** MENU PRINCIPAL *****")
+    print("1 - Produtos Químicos")
+    print("2 - Medicamentos")
+    print("3 - Funcionários")
+    print("4 - Relatórios")
+    opcao=raw_input("Digite a opção desejada: ")
+    if opcao=="1":
+        lista='PRODUTO'
+        menu_produto(lista)
+    if opcao=="2":
+        lista='MEDICAMENTO'
+        menu_medicamento(lista)
+    if opcao=="3":
+        lista='FUNCIONARIO'
+        menu_funcionario(lista)
+    if opcao=="4":
+        lista='RELATORIO'
+        menu_relatorio(lista)
+    menu_principal(lista)
 
-This is a basic picture viewer, using the scatter widget.
-'''
+def menu_produto(lista):
+    print("***** PRODUTOS *****")
+    menu_sec(lista)   
+def menu_medicamento(lista):
+    print("***** MEDICAMENTOS *****")
+    menu_sec(lista)         
+def menu_funcionario(lista):
+    print("***** FUNCIONÁRIOS *****")
+    menu_sec(lista)      
+def menu_relatorio(lista):
+    print("***** RELATÓRIOS *****")
+    print("Informa se um quantidade N de unidades comerciais de um dado medicamento pode ser ou nao")
+    print("fabricadaconsiderando o a disponibilidade de produtos quımicos necessarios e de funcionarios habilitados")
+    print("Informa os produtos quımicos e medicamentos a vencerem nos proximos 10 dias")
+    print("Informa qual medicamento possui o maior numero de funcionarios habilitados para sua producao,")
+    print("independentemente da existencia de produtos quımicos em estoque.")
+    menu_principal(lista)    
+def cadastro(lista):
+    print '***** CADASTRAR - ',lista,' *****'
+ 
+def busca(lista):
+    print '***** BUSCAR - ',lista,' *****'
 
-import kivy
-kivy.require('1.0.6')
+def atualiza(lista):
+    print '***** ATUALIZAR - ',lista,' *****'
+ 
+def remove(lista):
+    print '***** REMOVER - ',lista,' *****'
 
-from glob import glob
-from random import randint
-from os.path import join, dirname
-from kivy.app import App
-from kivy.logger import Logger
-from kivy.uix.scatter import Scatter
-from kivy.properties import StringProperty
-# FIXME this shouldn't be necessary
-from kivy.core.window import Window
+def menu_sec(lista):
+    print("1 - Cadastrar")
+    print("2 - Buscar")
+    print("3 - Atualizar")
+    print("4 - Remover")
+    opcao_s=raw_input("Digite a opção desejada: ")
+    if opcao_s=="1":
+        cadastro(lista)
+    if opcao_s=="2":
+        busca(lista)
+    if opcao_s=="3":
+        atualiza(lista)
+    if opcao_s=="4":
+        remove(lista)
+    menu_sec(lista) 
 
-
-class Picture(Scatter):
-    '''Picture is the class that will show the image with a white border and a
-    shadow. They are nothing here because almost everything is inside the
-    picture.kv. Check the rule named <Picture> inside the file, and you'll see
-    how the Picture() is really constructed and used.
-
-    The source property will be the filename to show.
-    '''
-
-    source = StringProperty(None)
-
-
-class PicturesApp(App):
-
-    def build(self):
-
-        # the root is created in pictures.kv
-        root = self.root
-
-        # get any files into images directory
-        curdir = dirname(__file__)
-        for filename in glob(join(curdir, 'images', '*')):
-            try:
-                # load the image
-                picture = Picture(source=filename, rotation=randint(-30,30))
-                # add to the main field
-                root.add_widget(picture)
-            except Exception, e:
-                Logger.exception('Pictures: Unable to load <%s>' % filename)
-
-    def on_pause(self):
-        return True
-
-
-if __name__ == '__main__':
-    PicturesApp().run()
-
+menu_principal(lista)
